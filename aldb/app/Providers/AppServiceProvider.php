301c7b55f704057ff7a7ddc0d69cef7c24840782
Andrew P Smith <Andrew.p.smith@warwick.ac.uk>
2019-11-25 16:48:19 +0000

    Added Auth0

--- aldb/app/Providers/AppServiceProvider.php
+++ aldb/app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Code Added by APS - 25 November 2019
+        $this->app->bind(
+            \Auth0\Login\Contract\Auth0UserRepository::class,
+            \Auth0\Login\Repository\Auth0UserRepository::class
+        );
     }
 }
