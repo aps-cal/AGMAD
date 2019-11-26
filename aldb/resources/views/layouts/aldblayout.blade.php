f3b21802f609e425002eede632d7cdd2ecc97ed8
Andrew P Smith <Andrew.p.smith@warwick.ac.uk>
2019-11-26 16:23:19 +0000

    Major Simplification

--- aldb/resources/views/layouts/aldblayout.blade.php
+++ aldb/resources/views/layouts/aldblayout.blade.php
@@ -8,10 +8,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'ALDB') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/aldb.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
